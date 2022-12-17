23f4ade0f522dda82617681d36e0a036be3a3969
Konstantin Kasachev <k.kasachev@yadro.com>
2022-12-17 19:52:58 +0400

    some basic examples

---
+++ first.py
@@ -0,0 +1,14 @@
+
+# print("Введите А")
+a = int(input('a = '))
+b = 1.23
+s = 'hello world'
+
+c = a ** 2 # возведение в степень
+
+print('{1} - {2} - {0}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+
+l = [1, 2, 3, 4]
+print (not 2 in l)
