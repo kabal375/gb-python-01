c94dffa906c5c4142df30ce5cf622b90777bc01c
Konstantin Kasachev <32961316+kabal375@users.noreply.github.com>
2023-01-31 14:43:28 +0300

    Hw06 (List comprehension, map, filter, lambda)
    * list comprehensions - RLE, sums of elements
    
    * map, filter, lambda - min_max_fract_parts
    
    ---------
    
    Co-authored-by: Konstantin Kasachev <k.kasachev@yadro.com>

--- HW03/1_sum_of_odd_elements.py
+++ HW03/1_sum_of_odd_elements.py
@@ -5,14 +5,8 @@
 
 from random import randint
 
-source_list = []
-odd_sum = 0
-
-for i in range(randint(3, 10)):
-    source_list.append(randint(-10, 10))
-    if i % 2 == 1:
-        odd_sum += source_list[i]
+source_list = [randint(-10, 10) for i in range(randint(3, 10))]
+odd_sum = sum([source_list[i] for i in range(len(source_list)) if i % 2 == 1])
 
 print(f'Исходный список: {source_list}')
 print(odd_sum)
-
